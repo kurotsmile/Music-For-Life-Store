22e2aeb3994e839326b96dda312bc341057ea7ce
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-07-08 19:19:10 +0700

    Update struct file

--- js/music.js
+++ js/music.js
@@ -1,9 +1,25 @@
-var list_artist=[];
-var list_year=[];
+class Music{
+    list_artist=[];
+    list_year=[];
 
-$(document).ready(function() {
+    onLoad(){
+        m.loadJs("js/songs.js","song");
+    }
 
-    function addOrUpdateObjectToList(list_obj, data_obj_add) {
+    loadJs(path_js, obj_call, func_call = "show") {
+        if(window[obj_call]!=null){
+            window[obj_call][func_call]();
+        }else{
+            $.getScript(path_js).done(function(script, textStatus) {
+                if(obj_call!=null) window[obj_call][func_call]();
+            })
+            .fail(function(jqxhr, settings, exception) {
+                console.log("Script loading failed: " + exception);
+            });
+        }
+    }
+    
+    addOrUpdateObjectToList(list_obj, data_obj_add) {
         var addedOrUpdate = false;
         for (var i = 0; i < list_obj.length; i++) {
             if (list_obj[i].name === data_obj_add.name && list_obj[i].lang === data_obj_add.lang) {
@@ -19,143 +35,59 @@ $(document).ready(function() {
         }
     }
 
-    $.getJSON("https://raw.githubusercontent.com/kurotsmile/Database-Store-Json/main/song.json", function(data) {
-        var songList = $("#song-list");
-        $.each(data.all_item, function(index, song) {
-            var obj_artist={"name":song.artist,"lang":song.lang};
-            addOrUpdateObjectToList(list_artist,obj_artist);
-
-            var obj_year={"name":song.year,"lang":song.lang};
-            addOrUpdateObjectToList(list_year,obj_year);
-            
-            var songItem = `<div role="button" class="song-item" data-src="${song.mp3}" data-title="${song.name}" data-artist="${song.artist}">
-                                <img src="images/avatar_music.png" alt="Avatar" class="song-avatar">
-                                <div class="song-title">${song.name}</div>
-                                <div class="song-artist">${song.artist}</div>
-                            </div>`;
-            songList.append(songItem);
-        });
-
-        $('.song-item').click(function() {
-            var songSrc = $(this).data('src');
-            var songTitle = $(this).data('title');
-            var songArtist = $(this).data('artist');
-
-            $('#player-song-title').text(songTitle);
-            $('#player-song-artist').text(songArtist);
-            $('#mp3-player').find('audio').remove();
-            $('#mp3-player').append(`<audio src="${songSrc}" id="audio-player" controls autoplay></audio>`);
-
-            var audioPlayer = $('#audio-player')[0];
-
-            $('#mp3-player').addClass('active');
+    donwload_artist(){
+        var data_download={};
+        data_download["all_item"]=m.list_artist;
+        data_download["collection"]='song_artist';
+        m.download_json(data_download,"song_artist.json");
+    }
     
-            $('#play-btn').off('click').click(function() {
-                if (audioPlayer.paused) {
-                    audioPlayer.play();
-                    $(this).html('&#10074;&#10074;');
-                } else {
-                    audioPlayer.pause();
-                    $(this).html('&#9654;');
-                }
-            });
+    donwload_year(){
+        var data_download={};
+        data_download["all_item"]=m.list_year;
+        data_download["collection"]='song_year';
+        m.download_json(data_download,"song_year.json");
+    }
+    
+    download_json(data,file_name){
+        var jsonString = JSON.stringify(data);
+        var blob = new Blob([jsonString], { type: "application/json" });
+    
+        var url = URL.createObjectURL(blob);
+        
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = file_name;
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+}
 
-            $('#prev-btn').off('click').click(function() {
-                // Implement previous song functionality if needed
-            });
+var m;
+$(document).ready(function() {
+    m=new Music();
+    m.onLoad();
 
-            // Next button functionality
-            $('#next-btn').off('click').click(function() {
-                // Implement next song functionality if needed
-            });
+     // Scroll to top button functionality
+     var scrollTopBtn = $("#scrollTopBtn");
 
-            // Set initial play/pause button state
-            $('#play-btn').html('&#10074;&#10074;'); // Pause icon
-        });
-
-        // Add click event for each song item info button
-        $('.song-info-btn').click(function() {
-            var songTitle = $(this).closest('.song-item').data('title');
-            var songArtist = $(this).closest('.song-item').data('artist');
-            var songSrc = $(this).closest('.song-item').data('src');
-
-            // Prepare data for SweetAlert2 modal
-            var htmlContent = `<table class="table table-bordered">
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">Title</th>
-                                            <td>${songTitle}</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">Artist</th>
-                                            <td>${songArtist}</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">Source</th>
-                                            <td>${songSrc}</td>
-                                        </tr>
-                                        <!-- Add more rows for other song details as needed -->
-                                    </tbody>
-                                </table>`;
-
-            // Display SweetAlert2 modal
-            Swal.fire({
-                title: 'Song Information',
-                html: htmlContent,
-                icon: 'info',
-                confirmButtonText: 'Close'
-            });
-        });
-    });
-
-    // Scroll to top button functionality
-    var scrollTopBtn = $("#scrollTopBtn");
-
-    $(window).scroll(function() {
-        var windowHeight = $(window).height();
-        var scrollHeight = $(document).height();
-        var scrollTop = $(window).scrollTop();
-
-        if (scrollTop > windowHeight / 2) {
-            scrollTopBtn.fadeIn();
-        } else {
-            scrollTopBtn.fadeOut();
-        }
-
-        var scrollRight = ($(window).width() - $('.container').offset().left - $('.container').width()) / 2 - 50; // 50px offset từ lề phải
-        scrollTopBtn.css('right', scrollRight);
-    });
-
-    scrollTopBtn.click(function() {
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
+     $(window).scroll(function() {
+         var windowHeight = $(window).height();
+         var scrollHeight = $(document).height();
+         var scrollTop = $(window).scrollTop();
+ 
+         if (scrollTop > windowHeight / 2) {
+             scrollTopBtn.fadeIn();
+         } else {
+             scrollTopBtn.fadeOut();
+         }
+ 
+         var scrollRight = ($(window).width() - $('.container').offset().left - $('.container').width()) / 2 - 50; // 50px offset từ lề phải
+         scrollTopBtn.css('right', scrollRight);
+     });
+ 
+     scrollTopBtn.click(function() {
+         $("html, body").animate({ scrollTop: 0 }, "slow");
+         return false;
+     });
 });
-
-
-function donwload_artist(){
-    var data_download={};
-    data_download["all_item"]=list_artist;
-    data_download["collection"]='song_artist';
-    download_json(data_download,"song_artist.json");
-}
-
-function donwload_year(){
-    var data_download={};
-    data_download["all_item"]=list_year;
-    data_download["collection"]='song_year';
-    download_json(list_year,"song_year.json");
-}
-
-function download_json(data,file_name){
-    var jsonString = JSON.stringify(data);
-    var blob = new Blob([jsonString], { type: "application/json" });
-
-    var url = URL.createObjectURL(blob);
-    
-    var a = document.createElement('a');
-    a.href = url;
-    a.download = file_name;
-    a.click();
-    URL.revokeObjectURL(url);
-}
--- js/songs.js
+++ js/songs.js
@@ -1,3 +1,94 @@
 class Songs{
+    show(){
+        $.getJSON("https://raw.githubusercontent.com/kurotsmile/Database-Store-Json/main/song.json", function(data) {
+            var songList = $("#song-list");
+            $.each(data.all_item, function(index, song) {
+                var obj_artist={"name":song.artist,"lang":song.lang};
+                m.addOrUpdateObjectToList(m.list_artist,obj_artist);
 
+                var obj_year={"name":song.year,"lang":song.lang};
+                m.addOrUpdateObjectToList(m.list_year,obj_year);
+                
+                var songItem = `<div role="button" class="song-item" data-src="${song.mp3}" data-title="${song.name}" data-artist="${song.artist}">
+                                    <img src="images/avatar_music.png" alt="Avatar" class="song-avatar">
+                                    <div class="song-title">${song.name}</div>
+                                    <div class="song-artist">${song.artist}</div>
+                                </div>`;
+                songList.append(songItem);
+            });
+
+            $('.song-item').click(function() {
+                var songSrc = $(this).data('src');
+                var songTitle = $(this).data('title');
+                var songArtist = $(this).data('artist');
+
+                $('#player-song-title').text(songTitle);
+                $('#player-song-artist').text(songArtist);
+                $('#mp3-player').find('audio').remove();
+                $('#mp3-player').append(`<audio src="${songSrc}" id="audio-player" controls autoplay></audio>`);
+
+                var audioPlayer = $('#audio-player')[0];
+
+                $('#mp3-player').addClass('active');
+        
+                $('#play-btn').off('click').click(function() {
+                    if (audioPlayer.paused) {
+                        audioPlayer.play();
+                        $(this).html('&#10074;&#10074;');
+                    } else {
+                        audioPlayer.pause();
+                        $(this).html('&#9654;');
+                    }
+                });
+
+                $('#prev-btn').off('click').click(function() {
+                    // Implement previous song functionality if needed
+                });
+
+                // Next button functionality
+                $('#next-btn').off('click').click(function() {
+                    // Implement next song functionality if needed
+                });
+
+                // Set initial play/pause button state
+                $('#play-btn').html('&#10074;&#10074;'); // Pause icon
+            });
+
+            // Add click event for each song item info button
+            $('.song-info-btn').click(function() {
+                var songTitle = $(this).closest('.song-item').data('title');
+                var songArtist = $(this).closest('.song-item').data('artist');
+                var songSrc = $(this).closest('.song-item').data('src');
+
+                // Prepare data for SweetAlert2 modal
+                var htmlContent = `<table class="table table-bordered">
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">Title</th>
+                                                <td>${songTitle}</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">Artist</th>
+                                                <td>${songArtist}</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">Source</th>
+                                                <td>${songSrc}</td>
+                                            </tr>
+                                            <!-- Add more rows for other song details as needed -->
+                                        </tbody>
+                                    </table>`;
+
+                // Display SweetAlert2 modal
+                Swal.fire({
+                    title: 'Song Information',
+                    html: htmlContent,
+                    icon: 'info',
+                    confirmButtonText: 'Close'
+                });
+            });
+        });
+    }
 }
+var song=new Songs();
+m.song=song;
