4f9007278c5bf4aaebbf9b56c1b260b0275fd325
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-07-09 23:05:47 +0700

    Add box sub menu

--- js/songs.js
+++ js/songs.js
@@ -6,6 +6,7 @@ class Songs{
 
     emp_list_song=null;
     lang="all";
+    box_info_menu_cur="none";
 
     show(){
         var container=$("#container");
@@ -54,38 +55,35 @@ class Songs{
                                     <img src="images/avatar_music.png" alt="Avatar" class="song-avatar">
                                     <div class="song-title">${song.name}</div>
                                     <div class="song-artist">${song.artist}</div>
-                                    <div class="btninfo btn-extension"><i class="fas fa-info-circle"></i></div>
-                                    <div class="btnvideo btn-extension"><i class="fab fa-youtube"></i></div>
+                                    
                                     <div class="btnplay btn-extension"><i class="fas fa-play-circle"></i></div>
+                                    <div class="btndownload btn-extension"><i class="fas fa-arrow-alt-circle-down"></i></div>
                                 </div>`);
-                var btn_info=$(songItem).find(".btninfo");
-                $(btn_info).click(()=>{
-                    var lyrics='';
-                    var containsHtmlTags = /<\/?[a-z][\s\S]*>/i.test(song.lyrics);
-                    if(containsHtmlTags) lyrics=$(song.lyrics).text();
-                    else lyrics=song.lyrics;
-                    Swal.fire({
-                        icon:"info",
-                        title:song.name,
-                        text:lyrics,
-                        iconColor: cr.color_btn,
-                        confirmButtonColor: cr.color_btn
-                    });
-                    return false;
-                });
+                var btn_info=$('<div class="btninfo btn-extension"><i class="fas fa-info-circle"></i></div>');
 
-                var btn_video=$(songItem).find(".btnvideo");
-                $(btn_video).click(()=>{
-                    var html='<iframe width="100%" height="315" src="https://www.youtube.com/embed/'+m.song.getYouTubeVideoId(song.link_ytb)+'?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
-                    Swal.fire({
-                        icon:"info",
-                        title:song.name,
-                        html:html,
-                        iconColor: cr.color_btn,
-                        confirmButtonColor: cr.color_btn
-                    });
+                $(btn_info).click(()=>{
+                    m.song.showInfoByData(song);
                     return false;
                 });
+                $(songItem).append(btn_info);
+
+                if(song.lyrics!=null&&song.lyrics!=""){
+                    var btnlyrics=$('<div class="btnlyrics btn-extension"><i class="fas fa-font"></i></div>');
+                    $(btnlyrics).click(()=>{
+                        m.song.showlyrics(song);
+                        return false;
+                    });
+                    $(songItem).append(btnlyrics);
+                }
+
+                if(song.link_ytb!=null){
+                    var btn_video=$('<div class="btnvideo btn-extension"><i class="fab fa-youtube"></i></div>');
+                    $(btn_video).click(()=>{
+                        m.song.showVideo(song);
+                        return false;
+                    });
+                    $(songItem).append(btn_video);
+                }
 
                 $(m.song.emp_list_song).append(songItem);
             });
@@ -126,6 +124,113 @@ class Songs{
             });
 
             m.song.showListCountry();
+    }
+
+    showVideo(data){
+        m.song.box_info_menu_cur="video";
+        var html='<iframe width="100%" height="315" src="https://www.youtube.com/embed/'+m.song.getYouTubeVideoId(data.link_ytb)+'?autoplay=0" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+        html+='<div id="all_btn_dock"></div>';
+        Swal.fire({
+            icon:"info",
+            title:data.name,
+            html:html,
+            iconColor: cr.color_btn,
+            confirmButtonColor: cr.color_btn,
+            didOpen:()=>{
+                m.song.menuSubInfoBox(data);
+            }
+        });
+    }
+
+    showlyrics(data){
+        m.song.box_info_menu_cur="lyrics";
+        var lyrics='';
+        var containsHtmlTags = /<\/?[a-z][\s\S]*>/i.test(data.lyrics);
+        if(containsHtmlTags) lyrics=$(data.lyrics).text();
+        else lyrics=data.lyrics;
+        lyrics+='<div id="all_btn_dock"></div>';
+        Swal.fire({
+            icon:"info",
+            title:data.name,
+            html:lyrics,
+            iconColor: cr.color_btn,
+            confirmButtonColor: cr.color_btn,
+            didOpen:()=>{
+                m.song.menuSubInfoBox(data);
+            }
+        });
+    }
+
+    showInfoByData(data){
+        m.song.box_info_menu_cur="info";
+        var t_table_info='<table class="table table-striped table-hover table-responsive fs-9 w-100 text-break" style="text-align:left;width:100%">';
+        t_table_info+='<tbody>';
+        $.each(data,function(k,v){
+            if(v!=""&&v!=null){
+                if(k=="mp3") return true;
+                if(k=="lyrics") return true;
+                if(k=="avatar") return true;
+                t_table_info+=m.song.getValByKeyTable(k,v);
+            }
+        });
+        t_table_info+='</tbody>';
+        t_table_info+='</table>';
+        t_table_info+='<div id="all_btn_dock"></div>';
+        Swal.fire({
+            title:data.name,
+            html:t_table_info,
+            confirmButtonColor: cr.color_btn,
+            didOpen:()=>{
+                m.song.menuSubInfoBox(data);
+            }
+        });
+    }
+
+    menuSubInfoBox(data){
+        if(data.lyrics!=null){
+            var btn_lyrics=$('<button class="btn btn-sm btn-c btn-msg '+(m.song.box_info_menu_cur === "lyrics" ? "active" : "lyrics")+' m-1 animate__animated animate__bounceIn"><i class="fas fa-comment"></i></button>');
+            $(btn_lyrics).click(function(){m.song.showlyrics(data);});
+            $("#all_btn_dock").append(btn_lyrics);    
+        }
+
+        if(data.link_ytb!=null){
+            var btn_ytb=$('<button class="btn btn-sm btn-c btn-msg '+(m.song.box_info_menu_cur === "video" ? "active" : "lyrics")+' m-1 animate__animated animate__bounceIn"><i class="fab fa-youtube"></i></button>');
+            $(btn_ytb).click(function(){m.song.showVideo(data);});
+            $("#all_btn_dock").append(btn_ytb);    
+        }
+
+        var btn_info=$('<button class="btn btn-sm btn-c btn-msg '+(m.song.box_info_menu_cur === "info" ? "active" : "info")+' m-1 animate__animated animate__bounceIn"><i class="fas fa-info-circle"></i></button>');
+        $(btn_info).click(function(){m.song.showInfoByData(data);});
+        $("#all_btn_dock").append(btn_info);
+
+    }
+
+    getValByKeyTable(k,v){
+        var val='';
+        var html='';
+        var btn_extension='';
+        switch (k.toLowerCase()) {
+            case "link_ytb":
+                val=m.song.getYouTubeVideoId(v);
+                btn_extension='<a target="_blank" href="'+v+'" class="btn btn-sm btn-dark"><i class="fas fa-external-link-square-alt"></i></a>';
+                break;
+            case "email":
+                btn_extension='<a target="_blank" href="mailto://'+v+'" class="btn btn-sm btn-dark"><i class="fas fa-envelope"></i></a>';
+                val=v;
+                break;
+            default:
+                val=v;
+                break;
+        }
+
+        if(val!=''){
+            html='<tr>';
+                html+='<th scope="row"><i class="fas fa-info"></i> '+k+'</th>';
+                html+='<td>'+val+'</td>';
+                html+='<td>'+btn_extension+'</td>';
+            html+='</tr>';
+        }
+        return html;
     }
 
     getYouTubeVideoId(url) {
