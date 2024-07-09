d4239b4082d4a218bb8eac0f8ac040197eaaaade
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-07-09 14:42:51 +0700

    Update singer
    Update singer list

--- js/songs_artist.js
+++ js/songs_artist.js
@@ -1,16 +1,19 @@
 class Songs_Artist{
+    emp_list_artist="";
 
     show(){
         var container=$("#container");
-        $(container).html('Tran Thien Thanh');
+        $(container).html('');
+        this.emp_list_artist=$('<div class="song-list col-12 pl-3 pr-3" id="song-list"></div>');
+        $(container).append(this.emp_list_artist);
 
-        $.each(m.list_year,function(index,y){
-            var songItem = `<div role="button" class="song-item">
-            <img src="images/year_music.png" alt="Avatar" class="song-avatar">
-            <div class="song-title">${y.name}</div>
-            <div class="song-artist">${y.amount} (${y.lang})</div>
-            </div>`;
-            songList.append(songItem);
+        $.each(m.list_artist,function(index,a){
+            var artistItem = $(`<div role="button" class="song-item">
+            <img src="images/singer.png" alt="Avatar Artist" class="song-avatar">
+            <div class="song-title">${a.name}</div>
+            <div class="song-artist">${a.amount} (${a.lang})</div>
+            </div>`);
+            $(m.song_artist.emp_list_artist).append(artistItem);
         });
     }
 }
